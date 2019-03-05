c296712b0ae8f9cb41f441ce0b3d5aef79aaa251
Hengzhe Ding <916407217@qq.com>
2019-03-05 02:28:39 +0000

    for testing

--- app.js
+++ app.js
@@ -8,40 +8,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'ddjackson272',
-    database : 'join_us'
+    host : 'tutorial-db-web.cjb5il7njevi.us-east-2.rds.amazonaws.com',
+    user : 'tutorial_user',
+    database : 'sample',
+    password : 'Zhe12345!'
 })
 
 app.get("/", function(req, res){
-    var q = "select count(*) as count from users";
-    connection.query(q, function(err,results){
-        if (err) throw err;
-        var count = (results[0].count);
-        //res.send("We have " + count + " users in our database!")
-        res.render("home", {data: count});
-    })
-});
-
-app.get("/joke", function(req, res){
-    var joke = "HeiHei";
-    res.send(joke);
-})
-
-app.get("/random_num", function(req, res){
-    var num = Math.floor((Math.random() * 10) + 1);
-    res.send("Your lucky number is " + num);
+    res.send("new")
+    // var q = "select count(*) as count from sample";
+    // connection.query(q, function(err,results){
+    //     if (err) throw err;
+    //     var count = (results[0].count);
+    //     //res.send("We have " + count + " users in our database!")
+    //     res.render("home", {data: count});
+    // })
 });
 
 app.post("/register", function(req, res){
-    var person = {email: req.body.email};
-    connection.query('insert into users set ?', person, function(err, results){
+    var person = {name: req.body.Name, address: req.body.Address};
+    connection.query('insert into sample set ?', person, function(err, results){
         if (err) throw err;
         res.redirect("/");
     })
 })
 
 app.listen(8080, function(){
-    console.log("Server running on 8080!")
+    console.log("Server running!")
 });
 
