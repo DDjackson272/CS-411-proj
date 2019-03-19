465888286e9835da3198dcb86fca775d769d56d4
Hengzhe Ding <916407217@qq.com>
2019-03-18 21:19:11 -0500

    rm redundant package and function

--- Server/app.js
+++ Server/app.js
@@ -8,9 +8,6 @@ const userRoutes = require("./routes/user");
 const housingRoutes = require("./routes/housing");
 const bodyParser = require('body-parser');
 const {loginRequired, ensureCorrectUser} = require("./middleware/auth");
-const bcrypt = require("bcrypt");
-const db = require("./models/index");
-const flash = require("connect-flash");
 const PORT = 80;
 
 app.set("view engine", "ejs");
@@ -19,54 +16,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-app.use(require("express-session")({
-    secret: "cs411proj",
-    resave: false,
-    saveUninitialized: true
-}));
-
-app.use(flash());
-
-app.use(function(req, res, next){
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
-});
-
+// contains two sub-routes: /signup & /signin, both are POST requests
 app.use("/api/auth", authRoutes);
 
+// contains one sub-routes: /, GET requests
 app.use("/api/user", userRoutes);
 
+// contains two sub-routes: / and /housing_id,
+// / has GET and POST request and /housing_id has GET and DELETE request
 app.use("/api/user/:id/housing", housingRoutes);
-
-// add encryption of password to not show them explicitly in database
-// used for un-restful api
-
-// app.post("/register", async function(req, res){
-//     let person = {email: req.body.email,
-//                 username: req.body.username,
-//                 img: req.body.img,
-//                 password: req.body.password};
-//
-//     person.password = await bcrypt.hash(person.password, 10);
-//
-//     db.query('insert into User set ?', person, function(err){
-//         if (err) {
-//             if (err.errno === 1062) {
-//                 // alert("This email address has been registered! Try another one.");
-//                 req.flash("error", "This email address has been registered! Try another one.");
-//                 res.redirect("/");
-//                 // console.log("This email address has been registered! Try another one.");
-//             } else {
-//                 req.flash("error", err.message);
-//                 res.redirect("/");
-//             }
-//         } else {
-//             req.flash("success", "Register successfully!");
-//             res.redirect("/");
-//         }
-//     })
-// });
 
 app.use(function(req, res, next){
     return next({
