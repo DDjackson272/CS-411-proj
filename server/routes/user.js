1821b732482c6cc0cb1a646e3ac43fd16c233189
Hengzhe Ding <916407217@qq.com>
2019-03-27 15:50:52 -0500

    add searching query

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,31 @@ const router = express.Router();
 const db = require("../models/index");
 const { signup, signin } = require("../handlers/auth");
 
+// /api/user/signup
 router.post("/signup", signup);
+
+// /api/user/search/:keyword
+router.get("/search/:keyword", function(req, res, next){
+    let searchUser = `select * from User` +
+        ` Where username LIKE "%${req.params.keyword}%"`;
+    db.query(searchUser, function(err, results){
+        if(err) {
+            return next({
+                status: 400,
+                message: err.message
+            });
+        } else {
+            return res.status(200).json(results);
+        }
+    });
+
+});
+
+// /api/user/signin
 router.post("/signin", signin);
-router.get("/", function(req, res){
+
+// /api/user/
+router.get("/", function(req, res, next){
     let allData = "select * from User;";
     db.query(allData, function(err, results){
         if (err) {
