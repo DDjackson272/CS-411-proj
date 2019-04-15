b2a6afc83ab840dee3484a8a2229baf87d8ff14e
Hengzhe Ding <916407217@qq.com>
2019-04-15 15:55:16 -0500

    refactor data.py & recommend.py

--- data/data.py
+++ data/data.py
@@ -2,9 +2,6 @@ import pymysql
 import csv
 import random
 from passlib.hash import bcrypt
-import pandas as pd
-import numpy as np
-from sklearn.cluster import KMeans
 from get_feature import get_data_from_db, get_feature_db
 from recommend import k_means_recommendation
 
@@ -14,24 +11,26 @@ from recommend import k_means_recommendation
 INITIAL_DATA_PATH = "./initial"
 FILE_PATH = "./FromDB"
 
+
 def gen_history_csv():
     history_tuple_list = list()
     # number of user, initially 18
     for i in range(1, 19):
         # number of housing, initially 20
-        for j in range(1,21):
+        for j in range(1, 21):
             visited_house_id = random.randint(1, 10)
             if visited_house_id <= 5:
                 history_tuple_list.append((i, j))
-    
-    with open(INITIAL_DATA_PATH+"/history.csv", "w") as csv_file:
+
+    with open(INITIAL_DATA_PATH + "/history.csv", "w") as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
         for tup in history_tuple_list:
             writer.writerow([tup[0]] + [tup[1]])
 
+
 def drop_all_tables(conn):
-    drop_list = ["Comment", "Recommend", "Housing", "Activity", "User", 
-    "HousingFeature", "History", "Sentiment"]
+    drop_list = ["Comment", "Recommend", "Housing", "Activity", "User",
+                 "HousingFeature", "History", "Sentiment"]
     cur = conn.cursor()
     for item in drop_list:
         drop_query = "drop table %s;" % item
@@ -41,96 +40,97 @@ def drop_all_tables(conn):
         except:
             print("Table %s is not in the database, no need to drop!" % item)
 
+
 def create_all_tables(conn):
     cur = conn.cursor()
 
-    query_user = "create table User (" +\
-    "user_id int NOT NULL AUTO_INCREMENT," +\
-    "email varchar(255) NOT NULL UNIQUE," +\
-    "username varchar(255) NOT NULL UNIQUE," +\
-    "password varchar(255) NOT NULL," +\
-    "img varchar(1024) NOT NULL," +\
-    "PRIMARY KEY (user_id)" +\
-    ");"
+    query_user = "create table User (" + \
+                 "user_id int NOT NULL AUTO_INCREMENT," + \
+                 "email varchar(255) NOT NULL UNIQUE," + \
+                 "username varchar(255) NOT NULL UNIQUE," + \
+                 "password varchar(255) NOT NULL," + \
+                 "img varchar(1024) NOT NULL," + \
+                 "PRIMARY KEY (user_id)" + \
+                 ");"
 
-    query_housing = "create table Housing (" +\
-    "housing_id int NOT NULL AUTO_INCREMENT," +\
-    "housing_name varchar(255) NOT NULL,"+\
-    "housing_username varchar(255) NOT NULL," +\
-    "address varchar(255) NOT NULL," +\
-    "city varchar(255) NOT NULL," +\
-    "housing_type varchar(255) NOT NULL," +\
-    "description varchar(1024) NOT NULL, " +\
-    "img_url varchar(1024) NOT NULL, " +\
-    "PRIMARY KEY (housing_id)," +\
-    "FOREIGN KEY (housing_username) REFERENCES User (username)" +\
-    ");"
+    query_housing = "create table Housing (" + \
+                    "housing_id int NOT NULL AUTO_INCREMENT," + \
+                    "housing_name varchar(255) NOT NULL," + \
+                    "housing_username varchar(255) NOT NULL," + \
+                    "address varchar(255) NOT NULL," + \
+                    "city varchar(255) NOT NULL," + \
+                    "housing_type varchar(255) NOT NULL," + \
+                    "description varchar(1024) NOT NULL, " + \
+                    "img_url varchar(1024) NOT NULL, " + \
+                    "PRIMARY KEY (housing_id)," + \
+                    "FOREIGN KEY (housing_username) REFERENCES User (username)" + \
+                    ");"
 
-    query_activity = "create table Activity (" +\
-    "activity_id int NOT NULL AUTO_INCREMENT," +\
-    "activity_name varchar(255) NOT NULL," +\
-    "username varchar(255) NOT NULL," +\
-    "address varchar(255) NOT NULL," +\
-    "city varchar(255) NOT NULL," +\
-    "type varchar(255) NOT NULL," +\
-    "description varchar(1024) NOT NULL," +\
-    "date DATE NOT NULL," +\
-    "img_url varchar(1024) NOT NULL," +\
-    "PRIMARY KEY (activity_id), " +\
-    "FOREIGN KEY (username) REFERENCES User (username)" +\
-    ");"
+    query_activity = "create table Activity (" + \
+                     "activity_id int NOT NULL AUTO_INCREMENT," + \
+                     "activity_name varchar(255) NOT NULL," + \
+                     "username varchar(255) NOT NULL," + \
+                     "address varchar(255) NOT NULL," + \
+                     "city varchar(255) NOT NULL," + \
+                     "type varchar(255) NOT NULL," + \
+                     "description varchar(1024) NOT NULL," + \
+                     "date DATE NOT NULL," + \
+                     "img_url varchar(1024) NOT NULL," + \
+                     "PRIMARY KEY (activity_id), " + \
+                     "FOREIGN KEY (username) REFERENCES User (username)" + \
+                     ");"
 
-    query_comment = "create table Comment (" +\
-    "comment_id int NOT NULL AUTO_INCREMENT," +\
-    "comment_housing_id int NOT NULL, " +\
-    "comment_user_id int NOT NULL, " +\
-    "content varchar(1024) NOT NULL," +\
-    "PRIMARY KEY (comment_id)," +\
-    "FOREIGN KEY (comment_housing_id) REFERENCES Housing (housing_id)," +\
-    "FOREIGN KEY (comment_user_id) REFERENCES User (user_id)" +\
-    ");"
+    query_comment = "create table Comment (" + \
+                    "comment_id int NOT NULL AUTO_INCREMENT," + \
+                    "comment_housing_id int NOT NULL, " + \
+                    "comment_user_id int NOT NULL, " + \
+                    "content varchar(1024) NOT NULL," + \
+                    "PRIMARY KEY (comment_id)," + \
+                    "FOREIGN KEY (comment_housing_id) REFERENCES Housing (housing_id)," + \
+                    "FOREIGN KEY (comment_user_id) REFERENCES User (user_id)" + \
+                    ");"
 
-    query_recommend = "create table Recommend (" +\
-    "recommend_id int NOT NULL AUTO_INCREMENT," +\
-    "recommend_username varchar(255) NOT NULL, " +\
-    "recommend_housing_id int NOT NULL," +\
-    "PRIMARY KEY (recommend_id)," +\
-    "FOREIGN KEY (recommend_username) REFERENCES User (username)," +\
-    "FOREIGN KEY (recommend_housing_id) REFERENCES Housing (housing_id)" +\
-    ");"
+    query_recommend = "create table Recommend (" + \
+                      "recommend_id int NOT NULL AUTO_INCREMENT," + \
+                      "recommend_username varchar(255) NOT NULL, " + \
+                      "recommend_housing_id int NOT NULL," + \
+                      "PRIMARY KEY (recommend_id)," + \
+                      "FOREIGN KEY (recommend_username) REFERENCES User (username)," + \
+                      "FOREIGN KEY (recommend_housing_id) REFERENCES Housing (housing_id)" + \
+                      ");"
 
-    query_housing_feature = "create table HousingFeature (" +\
-    "housing_feature_id int NOT NULL AUTO_INCREMENT,"+\
-    "housing_feature_housing_id int NOT NULL,"+\
-    "parking int NOT NULL, "+\
-    "cooking int NOT NULL, "+\
-    "large_bed int NOT NULL, "+\
-    "PRIMARY KEY (housing_feature_id),"+\
-    "FOREIGN KEY (housing_feature_housing_id) REFERENCES Housing (housing_id)"+\
-    ");"
+    query_housing_feature = "create table HousingFeature (" + \
+                            "housing_feature_id int NOT NULL AUTO_INCREMENT," + \
+                            "housing_feature_housing_id int NOT NULL," + \
+                            "parking int NOT NULL, " + \
+                            "cooking int NOT NULL, " + \
+                            "large_bed int NOT NULL, " + \
+                            "PRIMARY KEY (housing_feature_id)," + \
+                            "FOREIGN KEY (housing_feature_housing_id) REFERENCES Housing (housing_id)" + \
+                            ");"
 
-    query_history = "create table History (" +\
-    "history_id int NOT NULL AUTO_INCREMENT,"+\
-    "history_user_id int NOT NULL,"+\
-    "history_housing_id int NOT NULL,"+\
-    "PRIMARY KEY (history_id), "+\
-    "FOREIGN KEY (history_user_id) REFERENCES User (user_id),"+\
-    "FOREIGN KEY (history_housing_id) REFERENCES Housing (housing_id)"+\
-    ");"
+    query_history = "create table History (" + \
+                    "history_id int NOT NULL AUTO_INCREMENT," + \
+                    "history_user_id int NOT NULL," + \
+                    "history_housing_id int NOT NULL," + \
+                    "PRIMARY KEY (history_id), " + \
+                    "FOREIGN KEY (history_user_id) REFERENCES User (user_id)," + \
+                    "FOREIGN KEY (history_housing_id) REFERENCES Housing (housing_id)" + \
+                    ");"
 
-    query_sentiment = "create table Sentiment ("+\
-    "sentiment_id int NOT NULL AUTO_INCREMENT, "+\
-    "sentiment_housing_id int NOT NULL, "+\
-    "positive_comment int NOT NULL, " +\
-    "neutral_comment int NOT NULL, "+\
-    "negative_comment int NOT NULL, "+\
-    "overall_comment varchar(255) NOT NULL, "+\
-    "PRIMARY KEY (sentiment_id), "+\
-    "FOREIGN KEY (sentiment_housing_id) REFERENCES Housing (housing_id)"+\
-    ");"
+    query_sentiment = "create table Sentiment (" + \
+                      "sentiment_id int NOT NULL AUTO_INCREMENT, " + \
+                      "sentiment_housing_id int NOT NULL, " + \
+                      "positive_comment int NOT NULL, " + \
+                      "neutral_comment int NOT NULL, " + \
+                      "negative_comment int NOT NULL, " + \
+                      "overall_comment varchar(255) NOT NULL, " + \
+                      "PRIMARY KEY (sentiment_id), " + \
+                      "FOREIGN KEY (sentiment_housing_id) REFERENCES Housing (housing_id)" + \
+                      ");"
 
-    query_list = [query_user, query_housing, query_activity, 
-    query_comment, query_recommend, query_housing_feature, query_history, query_sentiment]
+    query_list = [query_user, query_housing, query_activity,
+                  query_comment, query_recommend, query_housing_feature, query_history, query_sentiment]
 
     for item in query_list:
         cur.execute(item)
@@ -139,29 +139,28 @@ def create_all_tables(conn):
 
 def main():
     # local db
-    # conn = pymysql.connect(
-    #     host='localhost',
-    #     port=3306,
-    #     user='root',
-    #     passwd='CS411!!!',
-    #     db='cs411proj')
-
-
-    # # aws db
     conn = pymysql.connect(
-        host="tutorial-db-web.cjb5il7njevi.us-east-2.rds.amazonaws.com",
+        host='localhost',
         port=3306,
-        user="tutorial_user",
-        passwd="Zhe12345!",
-        db="sample"
-    )
+        user='root',
+        passwd='CS411!!!',
+        db='cs411proj')
+
+    # aws db
+    # conn = pymysql.connect(
+    #     host="tutorial-db-web.cjb5il7njevi.us-east-2.rds.amazonaws.com",
+    #     port=3306,
+    #     user="tutorial_user",
+    #     passwd="Zhe12345!",
+    #     db="sample"
+    # )
 
     gen_history_csv()
     drop_all_tables(conn)
     create_all_tables(conn)
 
     # add User to db
-    with open(INITIAL_DATA_PATH+"/user.csv", 'r', encoding="utf-8") as csvfile:
+    with open(INITIAL_DATA_PATH + "/user.csv", 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         cur = conn.cursor()
         for row in reader:
@@ -178,7 +177,7 @@ def main():
     print("User insertion done!")
 
     # add housing to db
-    with open(INITIAL_DATA_PATH+"/housing.csv", 'r', encoding="utf-8") as csvfile:
+    with open(INITIAL_DATA_PATH + "/housing.csv", 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         cur = conn.cursor()
         for row in reader:
@@ -189,32 +188,31 @@ def main():
             housing_type = row[4]
             description = row[5]
             img_url = row[6]
-            query = 'insert into Housing (housing_name, housing_username, address, city, '\
-            'housing_type, description, img_url) values ("%s", "%s", "%s", "%s", "%s", "%s", "%s")' % \
-            (housing_name, housing_username, address, city, housing_type, description, img_url)
+            query = 'insert into Housing (housing_name, housing_username, address, city, ' \
+                    'housing_type, description, img_url) values ("%s", "%s", "%s", "%s", "%s", "%s", "%s")' % \
+                    (housing_name, housing_username, address, city, housing_type, description, img_url)
             cur.execute(query)
 
     conn.commit()
     print("Housing insertion done!")
 
     # add comment to db
-    with open(INITIAL_DATA_PATH+"/comment.csv", 'r', encoding="utf-8") as csvfile:
+    with open(INITIAL_DATA_PATH + "/comment.csv", 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         cur = conn.cursor()
         for row in reader:
             comment_housing_id = row[0]
             comment_user_id = row[1]
             comment_content = row[2]
-            query = 'insert into Comment (comment_housing_id, comment_user_id, content) '\
-            'values ("%s", "%s", "%s")' % (int(comment_housing_id), int(comment_user_id), comment_content)
+            query = 'insert into Comment (comment_housing_id, comment_user_id, content) ' \
+                    'values ("%s", "%s", "%s")' % (int(comment_housing_id), int(comment_user_id), comment_content)
             cur.execute(query)
 
     conn.commit()
     print("Housing comment insertion done!")
 
-
     # add housing_feature to db
-    with open(INITIAL_DATA_PATH+"/housing_feature.csv", 'r', encoding="utf-8") as csvfile:
+    with open(INITIAL_DATA_PATH + "/housing_feature.csv", 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         cur = conn.cursor()
         for row in reader:
@@ -222,28 +220,28 @@ def main():
             parking = row[1]
             cooking = row[2]
             large_bed = row[3]
-            query = 'insert into HousingFeature (housing_feature_housing_id, parking, cooking, large_bed) '\
-            'values ("%s", "%s", "%s", "%s")' % (int(housing_feature_housing_id), int(parking), \
-                int(cooking), int(large_bed))
+            query = 'insert into HousingFeature (housing_feature_housing_id, parking, cooking, large_bed) ' \
+                    'values ("%s", "%s", "%s", "%s")' % (int(housing_feature_housing_id), int(parking), \
+                                                         int(cooking), int(large_bed))
             cur.execute(query)
     conn.commit()
     print("Housing feature insertion done!")
 
     # add history to db
-    with open(INITIAL_DATA_PATH+"/history.csv", 'r', encoding="utf-8") as csvfile:
+    with open(INITIAL_DATA_PATH + "/history.csv", 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         cur = conn.cursor()
         for row in reader:
             history_user_id = row[0]
             history_housing_id = row[1]
-            query = 'insert into History (history_user_id, history_housing_id) '\
-            'values ("%s", "%s")' % (int (history_user_id), int(history_housing_id))
+            query = 'insert into History (history_user_id, history_housing_id) ' \
+                    'values ("%s", "%s")' % (int(history_user_id), int(history_housing_id))
             cur.execute(query)
     conn.commit()
     print("History insertion done!")
 
     # add sentiment to db
-    with open(INITIAL_DATA_PATH+"/sentiment.csv", 'r', encoding="utf-8") as csvfile:
+    with open(INITIAL_DATA_PATH + "/sentiment.csv", 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         cur = conn.cursor()
         for row in reader:
@@ -252,10 +250,10 @@ def main():
             neutral_comment = row[2]
             negative_comment = row[3]
             overall_comment = row[4]
-            query = 'insert into Sentiment (sentiment_housing_id, positive_comment, '\
-            'neutral_comment, negative_comment, overall_comment) values (%s, %s, %s, %s, "%s")' %\
-            (int(sentiment_housing_id), int(positive_comment), 
-                int(neutral_comment), int(negative_comment), overall_comment)
+            query = 'insert into Sentiment (sentiment_housing_id, positive_comment, ' \
+                    'neutral_comment, negative_comment, overall_comment) values (%s, %s, %s, %s, "%s")' % \
+                    (int(sentiment_housing_id), int(positive_comment),
+                     int(neutral_comment), int(negative_comment), overall_comment)
             cur.execute(query)
     conn.commit()
     print("Sentiment insertion done!")
@@ -265,14 +263,14 @@ def main():
     k_means_recommendation()
 
     # add recommend_housing to db
-    with open(INITIAL_DATA_PATH+"/recommend.csv", 'r', encoding="utf-8") as csvfile:
+    with open(INITIAL_DATA_PATH + "/recommend.csv", 'r', encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         cur = conn.cursor()
         for row in reader:
             recommend_username = row[0]
             recommend_housing_id = row[1]
-            query = 'insert into Recommend (recommend_username, recommend_housing_id) values '\
-            '("%s", %s)' % (recommend_username, int(recommend_housing_id))
+            query = 'insert into Recommend (recommend_username, recommend_housing_id) values ' \
+                    '("%s", %s)' % (recommend_username, int(recommend_housing_id))
             cur.execute(query)
     conn.commit()
     print("Recommend insertion done!")
--- data/recommend.py
+++ data/recommend.py
@@ -9,105 +9,106 @@ FILE_PATH = "./FromDB"
 INITIAL_DATA_PATH = "./initial"
 CLUSTER_NUM = 2
 
+
 def k_means_recommendation():
-	user_id_name = dict()
-	user_favorite_type = dict()
-	user_favorite_housing = dict()
-	train_data = pd.read_csv(FILE_PATH+"/TrainingTable.csv")
-	user_table = pd.read_csv(FILE_PATH+"/User.csv")[["user_id", "username"]]
-	reference_house = pd.read_csv(FILE_PATH+"/FinalHousingFeature.csv")
-	user_list = list(set(train_data['history_user_id'].tolist()))
-	for user in user_list:
-		username = user_table[user_table["user_id"]==user]["username"].tolist()[0]
-		train_user_data = np.array(train_data[train_data['history_user_id']==user][
-			["housing_type", "parking", "cooking", "large_bed"]])
-		kmeans_user = KMeans(n_clusters=CLUSTER_NUM).fit(train_user_data)
-		user_favorite_type[username]=np.round(kmeans_user.cluster_centers_).tolist()
+    user_favorite_type = dict()
+    user_favorite_housing = dict()
+    train_data = pd.read_csv(FILE_PATH + "/TrainingTable.csv")
+    user_table = pd.read_csv(FILE_PATH + "/User.csv")[["user_id", "username"]]
+    reference_house = pd.read_csv(FILE_PATH + "/FinalHousingFeature.csv")
+    user_list = list(set(train_data['history_user_id'].tolist()))
+    for user in user_list:
+        username = user_table[user_table["user_id"] == user]["username"].tolist()[0]
+        train_user_data = np.array(train_data[train_data['history_user_id'] == user][
+                                       ["housing_type", "parking", "cooking", "large_bed"]])
+        k_means_user = KMeans(n_clusters=CLUSTER_NUM).fit(train_user_data)
+        user_favorite_type[username] = np.round(k_means_user.cluster_centers_).tolist()
 
-	for user in user_favorite_type:
-		for value in user_favorite_type[user]:
-			housing_type = int(value[0])
-			parking = int(value[1])
-			cooking = int(value[2])
-			large_bed = int(value[3])
-			housing_id_set = reference_house[(reference_house["housing_type"]==housing_type) &\
-			 (reference_house["parking"]==parking) & (reference_house["cooking"]==cooking) &\
-			 (reference_house["large_bed"]==large_bed)]["housing_id"].tolist()
-			try:
-				user_favorite_housing[user] += housing_id_set
-			except KeyError:
-				user_favorite_housing[user] = housing_id_set
-		user_favorite_housing[user] = list(set(user_favorite_housing[user]))
+    for user in user_favorite_type:
+        for value in user_favorite_type[user]:
+            housing_type = int(value[0])
+            parking = int(value[1])
+            cooking = int(value[2])
+            large_bed = int(value[3])
+            housing_id_set = reference_house[(reference_house["housing_type"] == housing_type) &
+                                             (reference_house["parking"] == parking) & (
+                                                         reference_house["cooking"] == cooking) &
+                                             (reference_house["large_bed"] == large_bed)]["housing_id"].tolist()
+            try:
+                user_favorite_housing[user] += housing_id_set
+            except KeyError:
+                user_favorite_housing[user] = housing_id_set
+        user_favorite_housing[user] = list(set(user_favorite_housing[user]))
 
-	with open(INITIAL_DATA_PATH+"/recommend.csv", "w") as csv_file:
-		writer = csv.writer(csv_file, delimiter=",")
-		for user in user_favorite_housing:
-			for housing_id in user_favorite_housing[user]:
-				writer.writerow([user] + [housing_id])
+    with open(INITIAL_DATA_PATH + "/recommend.csv", "w") as csv_file:
+        writer = csv.writer(csv_file, delimiter=",")
+        for user in user_favorite_housing:
+            for housing_id in user_favorite_housing[user]:
+                writer.writerow([user] + [housing_id])
+
 
 def overwrite_recommend_table_db(conn):
+    # first drop the table
+    cur = conn.cursor()
+    try:
+        drop_query = "drop table %s" % 'Recommend'
+        cur.execute(drop_query)
+        conn.commit()
+    except:
+        print("Recommend table has already been dropped!")
 
-	# first drop the table
-	cur = conn.cursor()
-	try:
-		drop_query = "drop table %s" % 'Recommend'
-		cur.execute(drop_query)
-		conn.commit()
-	except:
-		print("Recommend table has already been dropped!")
+    # then create the table
+    query_recommend = "create table Recommend (" + \
+                      "recommend_id int NOT NULL AUTO_INCREMENT," + \
+                      "recommend_username varchar(255) NOT NULL, " + \
+                      "recommend_housing_id int NOT NULL," + \
+                      "PRIMARY KEY (recommend_id)," + \
+                      "FOREIGN KEY (recommend_username) REFERENCES User (username)," + \
+                      "FOREIGN KEY (recommend_housing_id) REFERENCES Housing (housing_id)" + \
+                      ");"
 
-	# then create the table
-	query_recommend = "create table Recommend (" +\
-	"recommend_id int NOT NULL AUTO_INCREMENT," +\
-	"recommend_username varchar(255) NOT NULL, " +\
-	"recommend_housing_id int NOT NULL," +\
-	"PRIMARY KEY (recommend_id)," +\
-	"FOREIGN KEY (recommend_username) REFERENCES User (username)," +\
-	"FOREIGN KEY (recommend_housing_id) REFERENCES Housing (housing_id)" +\
-	");"
+    try:
+        cur.execute(query_recommend)
+        conn.commit()
+    except:
+        print("Recommend table already exists!")
 
-	try:
-		cur.execute(query_recommend)
-		conn.commit()
-	except:
-		print("Recommend table already exists!")
+    with open(INITIAL_DATA_PATH + "/recommend.csv", 'r', encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        cur = conn.cursor()
+        for row in reader:
+            recommend_username = row[0]
+            recommend_housing_id = row[1]
+            query = 'insert into Recommend (recommend_username, recommend_housing_id) values ' \
+                    '("%s", %s)' % (recommend_username, int(recommend_housing_id))
+            cur.execute(query)
+    conn.commit()
+    print("Recommend updating done!")
 
-	with open(INITIAL_DATA_PATH+"/recommend.csv", 'r', encoding="utf-8") as csvfile:
-		reader = csv.reader(csvfile)
-		cur = conn.cursor()
-		for row in reader:
-			recommend_username = row[0]
-			recommend_housing_id = row[1]
-			query = 'insert into Recommend (recommend_username, recommend_housing_id) values '\
-			'("%s", %s)' % (recommend_username, int(recommend_housing_id))
-			cur.execute(query)
-	conn.commit()
-	print("Recommend updating done!")
 
 def main():
-
-	# conn = pymysql.connect(
-	# host='localhost',
-	# port=3306,
-	# user='root',
-	# passwd='CS411!!!',
-	# db='cs411proj')
+    # local db
+    conn = pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        passwd='CS411!!!',
+        db='cs411proj')
 
     # # aws db
-    conn = pymysql.connect(
-        host="tutorial-db-web.cjb5il7njevi.us-east-2.rds.amazonaws.com",
-        port=3306,
-        user="tutorial_user",
-        passwd="Zhe12345!",
-        db="sample"
-    )
+    # conn = pymysql.connect(
+    #     host="tutorial-db-web.cjb5il7njevi.us-east-2.rds.amazonaws.com",
+    #     port=3306,
+    #     user="tutorial_user",
+    #     passwd="Zhe12345!",
+    #     db="sample"
+    # )
 
     get_data_from_db(conn)
     get_feature_db()
     k_means_recommendation()
     overwrite_recommend_table_db(conn)
 
+
 if __name__ == "__main__":
     main()
-
-
